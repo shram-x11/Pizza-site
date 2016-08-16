b798b681e1a853997c3b63227191d9525acbabc3
Stanislav Nikolaenko <z-payne@yandex.ru>
2016-08-16 22:08:24 +0300

    Ignore update

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://pizza-site/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://pizza-site/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/server/OpenServer/domains/Pizza-site/catalog/');
+define('DIR_SYSTEM', 'C:/server/OpenServer/domains/Pizza-site/system/');
+define('DIR_IMAGE', 'C:/server/OpenServer/domains/Pizza-site/image/');
+define('DIR_LANGUAGE', 'C:/server/OpenServer/domains/Pizza-site/catalog/language/');
+define('DIR_TEMPLATE', 'C:/server/OpenServer/domains/Pizza-site/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/server/OpenServer/domains/Pizza-site/system/config/');
+define('DIR_CACHE', 'C:/server/OpenServer/domains/Pizza-site/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/server/OpenServer/domains/Pizza-site/system/storage/download/');
+define('DIR_LOGS', 'C:/server/OpenServer/domains/Pizza-site/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/server/OpenServer/domains/Pizza-site/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/server/OpenServer/domains/Pizza-site/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '82.146.48.90');
+define('DB_USERNAME', 'shram');
+define('DB_PASSWORD', 'rockerx88');
+define('DB_DATABASE', 'work1');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
